d8dbd51febf2bd205c110eb29f53fde0c11aaa3b
cuongvu3009 <cuongvu3009@gmail.com>
2023-09-28 14:13:00 +0300

    feat: get businessSlug to redirect

--- src/app/feedback/end/page.tsx
+++ src/app/feedback/end/page.tsx
@@ -2,17 +2,23 @@
 
 import "./endfeedback.css";
 
+import { useRouter, useSearchParams } from "next/navigation";
+
 import Button from "@/components/shared/Button";
 import { NextPage } from "next";
 import { PiHeartStraightLight } from "react-icons/pi";
 import Title from "@/components/shared/Title";
 import TradeMark from "@/app/feedback/components/TradeMark";
-import { useRouter } from "next/navigation";
 
 const EndFeedBack: NextPage<{ params: { businessSlug: string } }> = ({
   params,
 }) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const businessSlug = searchParams.get("businessSlug");
+  const stripe_session_id = searchParams.get("session_id");
+
+  console.log(stripe_session_id);
 
   return (
     <>
@@ -30,9 +36,7 @@ const EndFeedBack: NextPage<{ params: { businessSlug: string } }> = ({
         <Button
           version="secondary"
           btnText="Submit another feedback"
-          onClick={() =>
-            router.push(`/feedback/service/${params.businessSlug}/`)
-          }
+          onClick={() => router.push(`/feedback/service/${businessSlug}/`)}
         />
         <TradeMark />
       </div>
