0bd8745973caddde83d8a773a12daac23208d35d
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-04 15:51:24 +0300

    feat: handle error instead console.log only

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useEffect } from "react";
-
 import DashboardResponses from "@/app/dashboard/components/responses/DashboardResponses";
 import DashboardScore from "@/app/dashboard/components/score/DashboardScore";
 import FeedbackChart from "@/app/dashboard/components/chart/FeedbackChart";
 import { NextPage } from "next";
+import React from "react";
 import Spinner from "@/components/shared/Spinner";
 import { getFetcher } from "@/utils/fetcher";
 import styles from "./dashboardInfo.module.css";
@@ -27,33 +26,46 @@ const DashboardInfo: NextPage = () => {
     setOrderFeedbacks(data.orderFeedback);
     setServiceFeedbacks(data.serviceFeedback);
   }
-  if (error) {
-    console.log(error);
-  }
+
   if (isLoading) {
     return <Spinner />;
   }
 
   return (
     <div className={`${styles["dashboard-info"]}`}>
-      <div className={`${styles["dashboard-content"]}`}>
-        <DashboardScore data={data?.serviceFeedback!} />
-        <DashboardScore data={data?.orderFeedback!} />
-        <DashboardResponses
-          data={(data?.serviceFeedback ?? []).concat(data?.orderFeedback ?? [])}
-          businessId={businessId as number}
-        />
-      </div>
-      <div className={`${styles["dashboard-chart"]}`}>
-        <div className={`${styles["chart"]}`}>
-          <h4>Service feedback</h4>
-          <FeedbackChart data={data?.serviceFeedback!} type="service" />
+      {isLoading && <Spinner />}
+
+      {error && (
+        <div className={styles.error}>
+          <p>Error loading data: {error.message}</p>
         </div>
-        <h4>Order feedback</h4>
-        <div className={`${styles["chart"]}`}>
-          <FeedbackChart data={data?.orderFeedback!} type="order" />
+      )}
+
+      {data && (
+        <div className={`${styles["dashboard-content"]}`}>
+          <DashboardScore data={data?.serviceFeedback!} />
+          <DashboardScore data={data?.orderFeedback!} />
+          <DashboardResponses
+            data={(data?.serviceFeedback ?? []).concat(
+              data?.orderFeedback ?? []
+            )}
+            businessId={businessId as number}
+          />
         </div>
-      </div>
+      )}
+
+      {data && (
+        <div className={`${styles["dashboard-chart"]}`}>
+          <div className={`${styles["chart"]}`}>
+            <h4>Service feedback</h4>
+            <FeedbackChart data={data?.serviceFeedback!} type="service" />
+          </div>
+          <h4>Order feedback</h4>
+          <div className={`${styles["chart"]}`}>
+            <FeedbackChart data={data?.orderFeedback!} type="order" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
