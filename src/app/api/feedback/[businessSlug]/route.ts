1ab6781d9d38c4cf0b2a6aaf21d35d5317234f88
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-04 14:17:07 +0300

    feat: add pagination for api get feedbacks

--- src/app/api/feedback/[businessSlug]/route.ts
+++ src/app/api/feedback/[businessSlug]/route.ts
@@ -1,10 +1,18 @@
+import { parse } from "querystring";
 import prisma from "@/lib/prisma";
 
 export async function GET(
   req: Request,
-  { params }: { params: { businessSlug: string } }
+  { params, query }: { params: { businessSlug: string }; query: string }
 ) {
   const businessSlug = params.businessSlug;
+
+  // Parse query parameters
+  const { page, perPage } = parse(query);
+
+  // Convert page and perPage to numbers (you may want to add validation)
+  const currentPage = parseInt(page as string, 10) || 1;
+  const itemsPerPage = parseInt(perPage as string, 10) || 5; // Default to 5 items each category per page
 
   try {
     const serviceFeedback = await prisma.feedback.findMany({
@@ -15,6 +23,8 @@ export async function GET(
       orderBy: {
         createdAt: "desc",
       },
+      skip: (currentPage - 1) * itemsPerPage,
+      take: itemsPerPage,
     });
 
     const orderFeedback = await prisma.feedback.findMany({
@@ -25,6 +35,8 @@ export async function GET(
       orderBy: {
         createdAt: "desc",
       },
+      skip: (currentPage - 1) * itemsPerPage,
+      take: itemsPerPage,
     });
 
     return Response.json({
