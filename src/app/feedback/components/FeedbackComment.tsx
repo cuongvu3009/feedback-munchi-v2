189bb30f3d5f0ed54ba16aab9936ce2beaee5bc6
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-05 11:50:09 +0300

    feat: styling comment box, and change ref > state

--- src/app/feedback/components/FeedbackComment.tsx
+++ src/app/feedback/components/FeedbackComment.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 import Button from "@/components/shared/Button";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
@@ -16,48 +16,30 @@ const FeedbackComment: React.FC<CommentProps> = ({
   emojiService,
   emojiOrder,
 }) => {
-  const { getItem, setItem, removeItem } = useLocalStorage();
+  const { setItem } = useLocalStorage();
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const commentInputRef = useRef<HTMLInputElement>(null);
+  let [comment, setComment] = useState("");
 
   const openPopup = () => {
     setIsPopupOpen(true);
-    if (commentInputRef.current) {
-      commentInputRef.current.focus();
-    }
   };
 
   const closePopup = () => {
     setIsPopupOpen(false);
   };
 
-  useEffect(() => {
-    const storedComment = getItem(storageKey);
-    if (storedComment && commentInputRef.current) {
-      commentInputRef.current.value = storedComment;
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [storageKey]);
-
-  useEffect(() => {
-    removeItem(storageKey);
-    setIsFormSubmitted(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [emojiService, emojiOrder]);
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const comment = commentInputRef.current?.value || "";
     if (!comment.trim()) {
       return;
     }
 
     setItem(storageKey, comment);
-
-    if (commentInputRef.current) {
-      commentInputRef.current.value = "";
-    }
 
     closePopup();
 
@@ -82,11 +64,12 @@ const FeedbackComment: React.FC<CommentProps> = ({
         <div className="popup">
           <form className="popup-container" onSubmit={handleSubmit}>
             <h3 className="popup-comment">Add a comment for the restaurant</h3>
-            <input
-              type="text"
+
+            <textarea
               placeholder="Your comment here..."
-              ref={commentInputRef}
-            />
+              value={comment}
+              onChange={handleCommentChange}
+            ></textarea>
             <Button type="submit" version="primary" btnText="Save" />
             <Button onClick={closePopup} version="secondary" btnText="Cancel" />
           </form>
