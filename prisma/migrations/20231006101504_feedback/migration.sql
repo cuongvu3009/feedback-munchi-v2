5754525f7b7f759fe639850621e890e8d657600d
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-06 13:15:09 +0300

    feat: not using enum in prisma but string

--- prisma/migrations/20231006101504_feedback/migration.sql
+++ prisma/migrations/20231006101504_feedback/migration.sql
@@ -1,6 +1,3 @@
--- CreateEnum
-CREATE TYPE "Type" AS ENUM ('SERVICE', 'ORDER');
-
 -- CreateTable
 CREATE TABLE "Feedback" (
     "id" SERIAL NOT NULL,
@@ -10,7 +7,7 @@ CREATE TABLE "Feedback" (
     "tags" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "type" "Type" NOT NULL,
+    "type" TEXT NOT NULL,
 
     CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
 );
