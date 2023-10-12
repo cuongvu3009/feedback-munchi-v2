67c3d7bad41ca57581b1e5de8bc572f1bba0338d
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-12 09:40:06 +0300

    fix: The root value has an unexpected property

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // Add any other domains you need
   },
-  api: {
-    externalResolver: true,
+  i18n: {
+    locales: ["en", "es", "fi"], // Define your supported locales
+    defaultLocale: "en", // Set your default locale
   },
 };
 
