e79462460e16ab5e0a334ccc00c55ad0796cdc89
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-12 13:53:22 +0300

    style: highlight when link is active

--- src/app/dashboard/components/sidebar/Sidebar.tsx
+++ src/app/dashboard/components/sidebar/Sidebar.tsx
@@ -12,10 +12,11 @@ import LogoutBtn from "@/app/dashboard/components/LogoutBtn";
 import { SidebarProps } from "@/types/dashboard.types";
 import TradeMark from "@/app/feedback/components/TradeMark";
 import styles from "./sidebar.module.css";
+import { usePathname } from "next/navigation";
 
 const Sidebar: React.FC<SidebarProps> = ({ business }) => {
   const [sidebarOpen, setSideBarOpen] = useState(true);
-
+  const pathName = usePathname();
   const handleViewSidebar = () => {
     setSideBarOpen(!sidebarOpen);
   };
@@ -44,25 +45,38 @@ const Sidebar: React.FC<SidebarProps> = ({ business }) => {
             <>
               <Link
                 href={`/dashboard/admin`}
-                className={`${styles["sidebar-link"]}`}
+                // className={`${styles["sidebar-link"]}`}
+                className={`${styles["sidebar-link"]} ${
+                  pathName == "/dashboard/admin" ? styles.activeLink : ""
+                }`}
               >
                 Dashboard
               </Link>
               <Link
                 href={`/dashboard/admin/responses`}
-                className={`${styles["sidebar-link"]}`}
+                className={`${styles["sidebar-link"]} ${
+                  pathName == "/dashboard/admin/responses"
+                    ? styles.activeLink
+                    : ""
+                }`}
               >
                 Responses
               </Link>
               <Link
                 href={`/dashboard/admin/tips`}
-                className={`${styles["sidebar-link"]}`}
+                className={`${styles["sidebar-link"]} ${
+                  pathName == "/dashboard/admin/tips" ? styles.activeLink : ""
+                }`}
               >
                 Tips
               </Link>
               <Link
                 href={`/dashboard/admin/settings`}
-                className={`${styles["sidebar-link"]}`}
+                className={`${styles["sidebar-link"]} ${
+                  pathName == "/dashboard/admin/settings"
+                    ? styles.activeLink
+                    : ""
+                }`}
               >
                 Settings
               </Link>
