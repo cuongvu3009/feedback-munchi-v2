a344426d6cac4903c51bd79ec3499f0b006d3a5a
cuongvu3009 <cuongvu3009@gmail.com>
2023-10-16 12:55:29 +0300

    chore: separate login component from layout

---
+++ src/app/dashboard/components/login/Login.tsx
@@ -0,0 +1,97 @@
+import React, { useRef, useState } from "react";
+
+import { API_BASE_URL } from "@/utils/constantAPI";
+import { ApiResponseLogin } from "@/types/dashboard.types";
+import Cookies from "js-cookie";
+import Spinner from "@/components/shared/Spinner";
+import TradeMark from "@/app/feedback/components/TradeMark";
+import axios from "axios";
+import styles from "./login.module.css";
+import { toast } from "react-toastify";
+
+const Login: React.FC = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [response, setResponse] = useState<ApiResponseLogin>({
+    data: null,
+    loading: false,
+    error: null,
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (email && password) {
+      setResponse({ data: null, loading: true, error: null });
+
+      try {
+        const result = await axios({
+          method: "POST",
+          url: `${API_BASE_URL}/auth/`,
+          data: {
+            email,
+            password,
+          },
+        });
+
+        Cookies.set("user", JSON.stringify(result.data.result), { expires: 3 });
+        setResponse({ data: result.data.result, loading: false, error: null });
+        toast.success("Login successful!");
+        window.location.reload();
+      } catch (error: any) {
+        setResponse({ data: null, loading: false, error });
+        toast.error(error.message);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Dashboard Manager</h2>
+      <form className={`${styles["login-form"]}`} onSubmit={handleLogin}>
+        <label className={styles.label}>Email</label>
+        <input
+          className={styles.input}
+          type="email"
+          placeholder="your-email@email.com"
+          ref={emailRef}
+          required
+        />
+        <label className={styles.label}>Password</label>
+        <input
+          className={styles.input}
+          type={showPassword ? "text" : "password"}
+          placeholder="your password here..."
+          ref={passwordRef}
+          required
+        />
+
+        <label className={styles.passwordShow}>
+          <p>Show Password</p>
+          <input
+            type="checkbox"
+            checked={showPassword}
+            onChange={togglePasswordVisibility}
+          />
+        </label>
+
+        <button
+          className={styles.button}
+          type="submit"
+          disabled={response.loading}
+        >
+          {response.loading ? <Spinner /> : "Login"}
+        </button>
+      </form>
+      <TradeMark />
+    </div>
+  );
+};
+
+export default Login;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { API_BASE_URL } from "@/utils/constantAPI";
-import { ApiResponseLogin } from "@/types/dashboard.types";
 import BusinessSelection from "./components/BusinessSelection";
 import Cookies from "js-cookie";
+import Login from "./components/login/Login";
 import Sidebar from "@/app/dashboard/components/sidebar/Sidebar";
 import { SidebarProvider } from "@/context/SidebarContext";
 import Spinner from "@/components/shared/Spinner";
-import TradeMark from "@/app/feedback/components/TradeMark";
 import axios from "axios";
 import styles from "./dashboard.module.css";
-import { toast } from "react-toastify";
 import { useBusinessContext } from "@/context/BusinessContext";
 
 interface BusinessProps {
@@ -24,17 +22,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const { business } = useBusinessContext();
   const [isLoading, setIsLoading] = useState(false);
   const [businesses, setBusinesses] = useState<BusinessProps[]>([]);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const [response, setResponse] = useState<ApiResponseLogin>({
-    data: null,
-    loading: false,
-    error: null,
-  });
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
-  };
+
   const [isUser, setIsUser] = useState(false);
 
   useEffect(() => {
@@ -65,35 +53,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     }
   }, []);
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const email = emailRef.current?.value;
-    const password = passwordRef.current?.value;
-
-    if (email && password) {
-      setResponse({ data: null, loading: true, error: null });
-
-      try {
-        const result = await axios({
-          method: "POST",
-          url: `${API_BASE_URL}/auth/`,
-          data: {
-            email,
-            password,
-          },
-        });
-
-        Cookies.set("user", JSON.stringify(result.data.result), { expires: 3 });
-        setResponse({ data: result.data.result, loading: false, error: null });
-        toast.success("Login successful!");
-        window.location.reload();
-      } catch (error: any) {
-        setResponse({ data: null, loading: false, error });
-        toast.error(error.message);
-      }
-    }
-  };
-
   return (
     <>
       {isLoading ? (
@@ -117,45 +76,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </SidebarProvider>
       ) : (
         // Login section
-        <div className={styles.container}>
-          <h2>Dashboard Manager</h2>
-          <form className={`${styles["login-form"]}`} onSubmit={handleLogin}>
-            <label className={styles.label}>Email</label>
-            <input
-              className={styles.input}
-              type="email"
-              placeholder="your-email@email.com"
-              ref={emailRef}
-              required
-            />
-            <label className={styles.label}>Password</label>
-            <input
-              className={styles.input}
-              type={showPassword ? "text" : "password"}
-              placeholder="your password here..."
-              ref={passwordRef}
-              required
-            />
-
-            <label className={styles.passwordShow}>
-              <p>Show Password</p>
-              <input
-                type="checkbox"
-                checked={showPassword}
-                onChange={togglePasswordVisibility}
-              />
-            </label>
-
-            <button
-              className={styles.button}
-              type="submit"
-              disabled={response.loading}
-            >
-              {response.loading ? <Spinner /> : "Login"}
-            </button>
-          </form>
-          <TradeMark />
-        </div>
+        <Login />
       )}
     </>
   );
